5a358a3bb698155d98a54727ed7dfbe5c799c172
Kristen Kulha <kristen.m.kulha@gmail.com>
2019-01-11 14:42:15 -0800

    Add jQuery for page navigation.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import '../src/sass/styles.scss';
 import '@fortawesome/fontawesome-free/js/all';
+import $ from 'jquery';
+
+$('#j-nav').on('click','a', function(event){ 
+  event.preventDefault();
+  let dest = $(this).attr("href");
+  if (dest === '#') {
+    window.scrollTo(0,0);
+  } else {
+    let elementOffset =  $(dest).offset();   
+    let navHeight = elementOffset.top - 65;
+    window.scrollTo(0, navHeight);
+  }   
+});
